711020d373db1e22d6cbea1b97e92257b3f28814
dv.badikov <dv.badikov@severstal.com>
2022-02-16 17:10:25 +0300

    Initial commit

---
+++ src/test/java/lesson_10_Test/BaseTest.java
@@ -0,0 +1,46 @@
+package lesson_10_Test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
+
+    @BeforeAll
+    static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        String user = System.getProperty("login");
+        String password = System.getProperty("password");
+        String remoteUrl = System.getProperty("remoteUrl");
+        String browser = System.getProperty("browser");
+        String version = System.getProperty("version");
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = String.format("https://%s:%s@%s", user, password, remoteUrl);
+        Configuration.browser = browser;
+        Configuration.browserVersion = version;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
+
